9b732de5e68114ed9aeacd1236dfc944e1ce95c2
Matthew Inman Cochrane <matthewinmancochrane@simple.com>
2021-02-08 12:44:54 -0800

    Initialize Spring Boot Application

---
+++ src/main/java/com/crowdstreet/assignment/CrowdstreetAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.crowdstreet.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdstreetAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdstreetAssignmentApplication.class, args);
+	}
+
+}
